4585658ef494920c6b2007b0ffe9bf3c1db92d6a
Vitor Manzano Villela Dias <160519411+vitormanzano@users.noreply.github.com>
2024-09-14 16:55:33 -0300

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,20 +1,17 @@
 class Solution(object):
     def isPalindrome(self, x):
-        global s
         s = 0
         aux = x
+        
         while aux>0:
+            
             k = aux%10
             s = s+k
             aux =aux//10
             if aux==0:
                 break
+                
             s =s*10
-           
-            
-        
-
-        
         if s==x:
             return True
         else:
