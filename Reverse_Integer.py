a7c1769912f3115db4a534097d1c62981f4fdf7e
Vitor Manzano Villela Dias <160519411+vitormanzano@users.noreply.github.com>
2024-09-14 16:51:41 -0300

    Create Reverse_Integer.py

---
+++ Reverse_Integer.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def reverse(self, x):
+        aux = x
+        if aux<0:
+            aux = aux*(-1)
+        sum = 0
+        while (aux>0):
+            k = aux%10
+            aux = aux//10
+            sum +=k
+            sum = sum*10
+        if x<0:
+            sum = sum *(-1)
+        sum = sum//10
+        if sum >  (2**31) -1 or sum  < -2 ** 31:
+            return 0
+        return sum
