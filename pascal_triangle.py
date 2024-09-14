b5f9a1c833f498a386048fdc9265dc62670b815e
Vitor Manzano Villela Dias <160519411+vitormanzano@users.noreply.github.com>
2024-09-14 18:04:10 -0300

    Update pascal_triangle.py

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -1,9 +1,11 @@
 class Solution(object):
     def generate(self, numRows):
         vet = []
+        
         for i in range(numRows):
             vet0=[]
             for j in range(i+1):
+                
                 if j==0 or j==i:
                     vet0.append(1)
                 else:
