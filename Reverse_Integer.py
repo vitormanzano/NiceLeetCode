3b5d0826048a83be72965c274b7be45eb48ecb7f
Vitor Manzano Villela Dias <160519411+vitormanzano@users.noreply.github.com>
2024-09-14 16:52:48 -0300

    Update Reverse_Integer.py

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,6 +1,5 @@
 class Solution(object):
     def reverse(self, x):
-        
         aux = x
         
         if aux<0:
@@ -17,6 +16,7 @@ class Solution(object):
             
         if x<0:
             sum = sum *(-1)
+            
         sum = sum//10
         if sum >  (2**31) -1 or sum  < -2 ** 31:
             return 0
