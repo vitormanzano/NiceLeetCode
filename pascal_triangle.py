6ac2bd11254b4db6488479ba886bf3b3af16cf93
Vitor Manzano Villela Dias <160519411+vitormanzano@users.noreply.github.com>
2024-09-15 17:26:48 -0300

    Update pascal_triangle.py

--- pascal_triangle.py
+++ pascal_triangle.py
@@ -9,7 +9,6 @@ class Solution(object):
                 if j==0 or j==i:
                     vet0.append(1)
                 else:
-            
                     vet0.append(vet[i-1][j-1]+vet[i-1][j])
                 
             vet.append(vet0)
