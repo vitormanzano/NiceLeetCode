76e3d0d579e34f3f7bbdf4694ab02bcef6e7b691
Vitor Manzano Villela Dias <160519411+vitormanzano@users.noreply.github.com>
2024-09-15 17:27:31 -0300

    Update Reverse_Integer.py

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -3,11 +3,8 @@ class Solution(object):
         aux = x
         if aux<0:
             aux = aux*(-1)
-            
         sum = 0
-        
-        while (aux>0):
-            
+        while (aux>0):  
             k = aux%10
             aux = aux//10
             sum +=k
