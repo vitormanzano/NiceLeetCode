20e0b1ce4117ad79c23ac3ba03c400c3475b86b8
Vitor Manzano Villela Dias <160519411+vitormanzano@users.noreply.github.com>
2024-11-01 20:03:50 -0300

    Update binarySearch.py

--- binarySearch.py
+++ binarySearch.py
@@ -1,1 +1,19 @@
+'''
+Resolução com 0 ms e 12,69 mb de memória. 100% e 80.24%
+'''
+
+class Solution(object):
+    def search(self, nums, target):
+        l, r = 0, len(nums)-1
+
+        while ( l <= r):
+            m = (l+r)//2
+
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                return m
+        return -1
 
